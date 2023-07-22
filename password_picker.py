ab0c78fb572a64f379ac4da7b2e9f34bdd88ffb6
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-07-22 19:59:19 +0530

    add

--- password_picker.py
+++ password_picker.py
@@ -4,17 +4,21 @@ import string
 nouns = ["cat", "dog", "book", "computer", "sun", "tree", "ocean", "mountain", "flower", "car", "house", "cup", "pizza", "pencil", "phone"]
 adjectives = ["happy", "beautiful", "clever", "mysterious", "brave", "vibrant", "peaceful", "energetic", "graceful", "adorable", "gloomy", "playful", "elegant", "delicious", "thoughtful"]
 
-
-
 another_PASSWORD=True
+
 while another_PASSWORD==True:
+
     noun=random.choice(nouns)
     adjective=random.choice(adjectives)
     number=random.randrange(1,100)
     special_character=random.choice(string.punctuation)
+
     password=noun+adjective+special_character+str(number)
+
     print("Your Password is...",password)
+
     option=input("Do you want another password ? (YES/NO)")
+
     if option.lower()== "yes":
         print("Your Password is...",password)
 
