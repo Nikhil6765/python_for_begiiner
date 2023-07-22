b1f8439bc3ecb68833fd4d012313a62581c201d2
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-07-22 18:39:30 +0530

    Added_quiz

--- quiz.py
+++ quiz.py
@@ -1,4 +1,4 @@
-print("Welcome to the Animal quiz")
+print("Welcome to the Animal Quiz")
 score=0
 def checkguess(guess,answer):
     global score
@@ -9,4 +9,11 @@ def checkguess(guess,answer):
 guess1=input("which bear lives in the north ?")
 
 checkguess(guess1,"polar bear")
-print(score)
+
+print("Your Score is : ",score)
+
+guess2=input("Which is the fastest Land Animal ? ")
+
+checkguess(guess2,"cheetah")
+
+print("Your Score is : ",score)
