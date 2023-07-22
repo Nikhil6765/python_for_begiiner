8420401aac54ee7f643066600ff4277c28b06f76
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-07-22 19:35:20 +0530

    add_password_picker

---
+++ password_picker.py
@@ -0,0 +1,13 @@
+import random 
+import string
+
+nouns = ["cat", "dog", "book", "computer", "sun", "tree", "ocean", "mountain", "flower", "car", "house", "cup", "pizza", "pencil", "phone"]
+adjectives = ["happy", "beautiful", "clever", "mysterious", "brave", "vibrant", "peaceful", "energetic", "graceful", "adorable", "gloomy", "playful", "elegant", "delicious", "thoughtful"]
+
+noun=random.choice(nouns)
+adjective=random.choice(adjectives)
+number=random.randrange(1,100)
+special_character=random.choice(string.punctuation)
+
+print(noun+adjective+special_character+str(number))
+      
