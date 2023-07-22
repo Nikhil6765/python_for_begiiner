610d5eb4cacd6aa10432b0c0d8def2cd41558d39
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-07-22 18:41:18 +0530

    add

--- quiz.py
+++ quiz.py
@@ -7,13 +7,13 @@ def checkguess(guess,answer):
         score=score+1
 
 guess1=input("which bear lives in the north ?")
-
 checkguess(guess1,"polar bear")
 
-print("Your Score is : ",score)
-
 guess2=input("Which is the fastest Land Animal ? ")
-
 checkguess(guess2,"cheetah")
 
+guess3=input("Which is the Largest Animal ? ")
+checkguess(guess3,"blue whale")
+
 print("Your Score is : ",score)
+
