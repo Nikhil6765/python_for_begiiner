09263da2a47d3117d70c80526108a84f64d8dd40
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-07-23 12:12:02 +0530

    add

--- nine_lives.py
+++ nine_lives.py
@@ -17,3 +17,26 @@ def update_clue(guessed_letter,secret_word,clue):
             clue[index]=guessed_letter
         index+=1
         
+while lives>0:
+    print(clue)
+    print('Lives left : '+heart_symbol*lives)
+    guess=input("guess a letter or whole word :")
+
+    if guess == secret_word:
+        guessed_word_correctly=True
+        break
+
+    if guess in secret_word:
+        update_clue(guess,secret_word,clue)
+
+    else:
+        print("Incorrect, You loose a life")
+        lives-=1
+
+if guessed_word_correctly: # it means guessd_word_correctlty == True :
+    print("you Won! The secret word was " + secret_word)
+
+else:
+    print("you lost! The secret word was "+ secret_word)
+    
+
