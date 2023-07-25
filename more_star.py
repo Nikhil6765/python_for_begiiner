ee553b5498d9453fbb978d4b888cc598644e5d0e
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-07-25 10:20:14 +0530

    added_more_stars

--- more_star.py
+++ more_star.py
@@ -1,4 +1,5 @@
 import turtle as t  
+from random import randint,random
 
 def draw_star(points,size,col,x,y):
     t.penup()
@@ -10,10 +11,17 @@ def draw_star(points,size,col,x,y):
     for i in range(points):
         t.forward(size)
         t.right(angle)
+        
     
     t.end_fill()
+   
+t.speed('fastest')   
+t.Screen().bgcolor('black')
+while True:
+    ranPts=randint(2,5)*2+1
+    ranSize=randint(10,50)
+    ranCol=(random(),random(),random())
+    ranX=randint(-350,300)
+    ranY=randint(-350,300)
+    draw_star(ranPts,ranSize,ranCol,ranX,ranY)
 
-
-t.Screen().bgcolor('dark blue')
-
-draw_star(5,50,'yellow',0,0)
