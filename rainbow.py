370b9daa2f3f7181a413c89605508da9d993dd2a
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-07-25 12:14:58 +0530

    add

---
+++ rainbow.py
@@ -0,0 +1,55 @@
+import turtle as t   
+import random
+
+
+def get_line_length():
+    choice=input('Enter Line Length (long,medium,short) :  ' )
+    if choice=='long':
+        line_length=250
+    elif choice =='medium':
+        line_length=200
+
+    else:
+        line_length=100
+
+    return line_length
+
+def get_line_width():
+    choice=input('enter line width (superthick, thick,thin) : ')
+    if choice=='superthick':
+        line_width=40
+
+    elif choice=='thick':
+        line_width=25
+    
+    else:
+        line_width=10
+    return line_width
+
+line_length=get_line_length()
+line_width=get_line_width()
+
+t.shape('turtle')
+t.fillcolor('green')
+t.bgcolor('black')
+t.speed('slow')
+t.pensize(line_width)
+
+
+def inside_window():
+    left_limit=(-t.window_width()/2)+100
+    right_limit=(-t.window_width()/2)-100
+    top_limit=(-t.window_height()/2)-100
+    bottom_limit=(-t.window_height()/2)+100
+    (x,y)=t.pos()
+    inside=left_limit<x<right_limit and bottom_limit <y<top_limit
+    return inside
+
+def move_turtle(line_length):
+    pen_colours=['red','orange','yellow','green','blue','purple']
+
+    t.pencolor(random.choice(pen_colours))
+    if inside_window():
+        angle=random.randint(0,180)
+        t.right(angle)
+        
